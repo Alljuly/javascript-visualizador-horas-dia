ae408d05e25913ef230fe937ae030cdd6621f3a5
Alljuly <ajsc.elsie@gmail.com>
2024-01-11 15:16:32 +0000

    fix: alterando src para as imagens no css

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         msg = "Tenha um bom dia!"
-        img.src = 'fotomanha.png'
+        img.src = 'assets/fotomanha.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         msg = "Aproveite sua Tarde"
-        img.src = 'fototarde.png'
+        img.src = 'assets/fototarde.png'
         document.body.style.background = '#b9846f'
     } else {
         msg = "Boa noite!"
-        img.src = 'fotonoite.png'
+        img.src = 'assets/fotonoite.png'
         document.body.style.background = '#515154'
     }
 
